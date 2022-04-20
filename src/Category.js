b34b6482cf338010fb2d0fb88a7d70ef5e7f2bf0
dongmin-Lim <ldm96216@gmail.com>
2022-04-20 19:30:23 +0900

    Update Category logic (카테고리 클릭 시 카테고리 명으로 링크 이동) Bug 발생 (카테고리를 두번 클릭해야 진입됌)

--- src/Category.js
+++ src/Category.js
@@ -1,42 +1,43 @@
 import styles from "./Category.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
+function Li({ text }) {
+  const [category, setCategory] = useState("");
+
+  const onClick = (e) => {
+    setCategory(e.target.innerText);
+  };
+  return (
+    <li className={styles.li} onClick={onClick}>
+      <Link to={`/${category}`}>{text}</Link>
+    </li>
+  );
+}
 
 function Category() {
   return (
     <div className={styles.main}>
       <ul className={styles.ul}>
-        <li className={styles.li}>
-          <Link to={"/"}>Category1</Link>
-        </li>
-        <li className={styles.li}>
-          <Link to={"/"}>Category2</Link>
-        </li>
-        <li className={styles.li}>
-          <Link to={"/"}>Category3</Link>
-        </li>
-        <li className={styles.li}>
-          <Link to={"/"}>Category4</Link>
-        </li>
-        <li className={styles.li}>
-          <Link to={"/"}>Category5</Link>
-        </li>
+        <Li text="Categoey1" />
+        <Li text="Categoey2" />
+        <Li text="Categoey3" />
+        <Li text="Categoey4" />
+        <Li text="Categoey5" />
       </ul>
       <ul className={styles.ul}>
-        <li className={styles.li}>
-          <Link to={"/"}>Category6</Link>
-        </li>
-        <li className={styles.li}>
-          <Link to={"/"}>Category7</Link>
-        </li>
-        <li className={styles.li}>
-          <Link to={"/"}>Category8</Link>
-        </li>
-        <li className={styles.li}>
-          <Link to={"/"}>Category9</Link>
-        </li>
-        <li className={styles.li}>
-          <Link to={"/"}>Category0</Link>
-        </li>
+        <Li text="Categoey6" />
+        <Li text="Categoey7" />
+        <Li text="Categoey8" />
+        <Li text="Categoey9" />
+        <Li text="Categoey10" />
+      </ul>
+      <ul className={styles.ul}>
+        <Li text="Categoey11" />
+        <Li text="Categoey12" />
+        <Li text="Categoey13" />
+        <Li />
+        <Li />
       </ul>
     </div>
   );
